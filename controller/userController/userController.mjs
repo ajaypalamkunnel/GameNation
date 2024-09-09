e37255be427452b338b543c28368ed8c9eef68c8
ajaypalamkunnel <ajaypalamkunnel45@gmail.com>
2024-09-09 11:20:23 +0530

    user header added with category listing

--- controller/userController/userController.mjs
+++ controller/userController/userController.mjs
@@ -1,7 +1,21 @@
-
+import category from "../../model/categoryScehema.mjs"
 
 
 export const home = async(req,res)=>{
-    res.render('user/home',{title:'Home',user:req.session.user})
+
+    try {
+        if(req.session.user){
+
+            const categories = await category.find({})
+
+            res.render('user/home',{title:'Home',user:req.session.user,categories})
+    
+        }else{
+            res.redirect('/login')
+        }
+        
+    } catch (error) {
+        
+    }
 }
 
