2a8647a09e247cc2228481108fc9f22a74284620
ajaypalamkunnel <ajaypalamkunnel45@gmail.com>
2024-10-22 09:45:02 +0530

    google authentication server change

--- services/auth.mjs
+++ services/auth.mjs
@@ -8,7 +8,8 @@ passport.use(
     new GoogleStrategy({
         clientID:process.env.GOOGLE_CLIENT_ID,
         clientSecret:process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL:'/auth/google/callback',
+        //callbackURL:'/auth/google/callback',
+        callbackURL:'https://gamenation.online/auth/google/callback',
         state:true
     },
 
